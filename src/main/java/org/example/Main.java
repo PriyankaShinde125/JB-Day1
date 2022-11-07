70a7634956eb97ceaddee084278e3daa90bd1277
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-07 14:54:08 +0530

    [Priyanka] Added Q1 Display a message solution

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        displayAMessage();
+    }
+   public static void displayAMessage()
+    {
+        System.out.println("Welcome to Bridgelabz LFP program");
+    }
+}
