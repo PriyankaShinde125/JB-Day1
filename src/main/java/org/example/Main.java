08ac045aca18a0b6f7a56b0abb6d3a9d70f8d712
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-07 15:34:06 +0530

    [Priyanka]Added Q2 Primitive data type default values solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,31 @@
 package org.example;
 
 public class Main {
+    static String stringObj;
+    static int  intObj;
+    static float floatObj;
+    static char charObj;
+    static double doubleObj;
+    static boolean booleanObj;
     public static void main(String[] args)
     {
         displayAMessage();
+        displayDefaultValues();
     }
-   public static void displayAMessage()
+    //Method to display Welcome message
+    public static void displayAMessage()
     {
         System.out.println("Welcome to Bridgelabz LFP program");
+    }
+    //Method to display default values of all data types
+    public static void displayDefaultValues()
+    {
+        System.out.println("Default values of primitive datatypes are as follows");
+        System.out.println("String="+stringObj);
+        System.out.println("int="+intObj);
+        System.out.println("float"+floatObj);
+        System.out.println("char="+charObj);
+        System.out.println("double="+doubleObj);
+        System.out.println("boolean="+booleanObj);
     }
 }
