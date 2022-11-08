61dbc45aed3ed28f807730f3f4a32c620501f2f1
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-08 10:22:04 +0530

    ADDED: code to print even array element

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,101 +3,104 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    static String stringObj;
-    static int  intObj;
-    static float floatObj;
-    static char charObj;
-    static double doubleObj;
-    static boolean booleanObj;
-    public static void main(String[] args)
-    {
+    String stringObj;
+    int intObj;
+    float floatObj;
+    char charObj;
+    double doubleObj;
+    boolean booleanObj;
+
+    public static void main(String[] args) {
+        Main mainObj = new Main();
         displayAMessage();
-        displayDefaultValues();
+        /*mainObj.displayDefaultValues();
         checkStringEquals();
         //findSumAndInvalidIntCount(args);
         getName(args);
-        checkLeapYear();
+        checkLeapYear();*/
+        int[] myArr = new int[]{1, 2, 3, 10, 5, 6, 7};
+        mainObj.findEven(myArr);
     }
+
+    private void findEven(int[] myArr) {
+        for (int i : myArr) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+        }
+    }
+
     //Method to display Welcome message
-    public static void displayAMessage()
-    {
+    public static void displayAMessage() {
         System.out.println("Welcome to Bridgelabz LFP program");
     }
+
     //Method to display default values of all data types
-    public static void displayDefaultValues()
-    {
+    public void displayDefaultValues() {
         System.out.println("Default values of primitive datatypes are as follows");
-        System.out.println("String="+stringObj);
-        System.out.println("int="+intObj);
-        System.out.println("float"+floatObj);
-        System.out.println("char="+charObj);
-        System.out.println("double="+doubleObj);
-        System.out.println("boolean="+booleanObj);
+        System.out.println("String=" + stringObj);
+        System.out.println("int=" + intObj);
+        System.out.println("float" + floatObj);
+        System.out.println("char=" + charObj);
+        System.out.println("double=" + doubleObj);
+        System.out.println("boolean=" + booleanObj);
     }
+
     //Method to check strings are equal or not
-    public static void checkStringEquals()
-    {
-        String stringOne="Bridgelabz";
-        String stringTwo="Bridgelabz";
-        if(stringOne.equals(stringTwo))
+    public static void checkStringEquals() {
+        String stringOne = "Bridgelabz";
+        String stringTwo = "Bridgelabz";
+        if (stringOne.equals(stringTwo))
             System.out.println("Both strings are equals");
         else
             System.out.println("Both strings are not equals");
     }
+
     //Method to find sum of command line integers and count invalid integers
-    public static void findSumAndInvalidIntCount(String[] args)
-    {
-       int sum=0;
-       int invalidIntCount=0;
-        for (int i=0;i< args.length;i++)
-        {
+    public static void findSumAndInvalidIntCount(String[] args) {
+        int sum = 0;
+        int invalidIntCount = 0;
+        for (int i = 0; i < args.length; i++) {
             try {
-                sum=sum+Integer.parseInt(args[i]);
-            }catch(NumberFormatException e)
-            {
+                sum = sum + Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
                 invalidIntCount++;
             }
         }
-        System.out.println("Sum of input number="+sum);
-        System.out.println("Number of invalid inputs="+invalidIntCount);
+        System.out.println("Sum of input number=" + sum);
+        System.out.println("Number of invalid inputs=" + invalidIntCount);
     }
+
     //Method to get name via CLA
-    public static void getName(String[] args)
-    {
-        String name=args[0];
-        System.out.println("Name accepted from command line is "+name);
+    public static void getName(String[] args) {
+        String name = args[0];
+        System.out.println("Name accepted from command line is " + name);
     }
+
     //Method to check leap year
-    public static void checkLeapYear()
-    {
+    public static void checkLeapYear() {
         int year;
-        boolean isLeap=false;
-        Scanner sc=new Scanner(System.in);
+        boolean isLeap = false;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter year");
-        year=sc.nextInt();
-        if(year>1582)
-        {
-            if (year % 4 == 0)
-            {
-                if (year % 100 == 0)
-                {
+        year = sc.nextInt();
+        if (year > 1582) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
                     if (year % 400 == 0)
                         isLeap = true;
                     else
                         isLeap = false;
-                }
-                else
+                } else
                     isLeap = true;
-            }
-            else
+            } else
                 isLeap = false;
-        }
-        else
+        } else
             System.out.println(" Invalid Year ");
 
         if (isLeap)
-            System.out.println(year+" is leap year");
+            System.out.println(year + " is leap year");
         else
-            System.out.println(year+" is not leap year");
+            System.out.println(year + " is not leap year");
     }
+
 }
